925af649281d4a04100d04c90180db3e939f7ea6
Francisco Jeronimo <franciscojeronimo1@hotmail.com>
2024-11-08 15:02:04 -0300

    ;

--- portifolio/envia.php
+++ portifolio/envia.php
@@ -1,19 +1,21 @@
 <?php
 
-  $nome = addslashes($_POST['nome']);
-  $email = addslashes($_POST['email']);
-  $mensagem = addslashes($_POST['mensagem']);
+$nome = addslashes($_POST['nome']);
+$email = addslashes($_POST['email']);
+$mensagem = addslashes($_POST['mensagem']);
 
-  $para = "catastomatw1@gmail.com";
-  $assunto = "Contato Pelo Portfolio";
+$para = "catastomatw1@gmail.com";
+$assunto = "Contato Pelo Portfolio";
 
-  $corpo = "Nome: ".$nome."\n"."E-mail: ".$email."\n"."Mensagem: ".$mensagem;
+$corpo = "Nome: " . $nome . "\n" . "E-mail: " . $email . "\n" . "Mensagem: " . $mensagem;
 
-  $cabeca = "From franciscojeronimo1@hotmail.com"."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversion();
+$cabeca = "From: franciscojeronimo1@hotmail.com\r\n" .
+          "Reply-To: " . $email . "\r\n" .
+          "X-Mailer: PHP/" . phpversion();
 
-  if(mail($para,$assunto,$corpo,$cabeca)) {
-    echo("Email enviado com sucesso!");
-  } else{
-    echo("Houve um erro ao enviar o email!");
-  }
+if (mail($para, $assunto, $corpo, $cabeca)) {
+    echo "Email enviado com sucesso!";
+} else {
+    echo "Houve um erro ao enviar o email!";
+}
 ?>
