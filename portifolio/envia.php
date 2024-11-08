b4e68455e66ec090ccc06839908df1f5223efff2
Francisco Jeronimo <franciscojeronimo1@hotmail.com>
2024-11-08 14:49:59 -0300

    php

---
+++ portifolio/envia.php
@@ -0,0 +1,19 @@
+<?php
+
+  $nome = addslashes($_POST['nome']);
+  $email = addslashes($_POST['email']);
+  $mensagem = addslashes($_POST['mensagem']);
+
+  $para = "catastomatw1@gmail.com";
+  $assunto = "Contato Pelo Portfolio";
+
+  $corpo = "Nome: ".$nome."\n"."E-mail: ".$email."\n"."Mensagem: ".$mensagem;
+
+  $cabeca = "From franciscojeronimo1@hotmail.com"."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversion();
+
+  if(mail($para,$assunto,$corpo,$cabeca)) {
+    echo("Email enviado com sucesso!");
+  } else{
+    echo("Houve um erro ao enviar o email!");
+  }
+?>
